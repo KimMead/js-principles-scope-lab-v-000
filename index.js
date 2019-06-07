83a55f95ddc1b104f961f97f1cff9212a5dd6f7c
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-07 15:05:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const leastFavoriteCustomer = undefined;
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
 };
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+  return bestCustomer;
+};
+
+function overwriteBestCustomer(customer) {
+  bestCustomer = customer;
+  return bestCustomer;
+};
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "customer";
+};
